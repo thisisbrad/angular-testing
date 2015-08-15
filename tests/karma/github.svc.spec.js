8901f467d46aa9d5145c792e81e8b7f58cacc532
Brad Beltowski <bbelowski@fullsail.com>
2015-08-15 18:06:31 -0400

    injected  to control responce of API call

--- tests/karma/github.svc.spec.js
+++ tests/karma/github.svc.spec.js
@@ -2,12 +2,19 @@ describe('myapp', function(){
 	beforeEach(module('myapp'))
 
 	describe('GithubSvc', function(){
-		it('fetches users', function(){
-			inject(function(GithubSvc){
+		it('fetches users', function(done){
+			inject(function(GithubSvc, $httpBackend){
+				//console.log(GithubSvc)
+				$httpBackend.when('GET', 'https://api.github.com/users')
+				.repsond(['user a', 'user b'])
+
 				GithubSvc.fetchUsers()
 				.then(function(users){
 					console.log(users)
+					done()
 				})
+
+				$httpBackend.flush()
 			})
 		})
 	})
